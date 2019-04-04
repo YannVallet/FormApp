99ed9bd87c7cf9c0ce7caf8436ccec87dff7349f
Ustariz Enzo <enzo.ustariz@hotmail.fr>
2019-04-04 15:14:26 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import FormInputs from './FormInputs';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <FormInputs />
+      </div>
+    );
+  }
+}
+
+export default App;
